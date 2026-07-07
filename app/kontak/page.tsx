551328ab31f760ccc4e65e839298ffaf04f68a2e
buanamedia <media.agussiahaan@gmail.com>
2026-07-07 05:34:32 +0000

    Update page.tsx

--- app/kontak/page.tsx
+++ app/kontak/page.tsx
@@ -57,8 +57,8 @@ export default function ContactPage() {
                   <p className="font-bold text-slate-900">Alamat Kantor Usaha</p>
                   <p className="text-slate-600 leading-relaxed mt-0.5">
                     Buanamedia Studio <br />
-                    Jl. Utama No. 45, Kompleks Bisnis Digital, <br />
-                    DKI Jakarta, Indonesia
+                    Jl. Buana Perkasa II Blok C4 No.35 RT.005 RW.004 <br />
+                    Pinang, Koita Tangerang, Indonesia
                   </p>
                 </div>
               </div>
